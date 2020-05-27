d4133db5f4632137bc2f5309e541c2cc2628b9c9
Vong3432 <vongnyuksoon2000@gmail.com>
2020-05-27 15:46:16 +0800

    Fix swap records and request.

--- item-details.php
+++ item-details.php
@@ -483,18 +483,48 @@
                 ?>
 
                 <!-- Swap -->
-                <?php 
-                  if($result->swap == 1){ 
-                ?>
-                  <div role="tabpanel" class="tab-pane" id="swap">
-                    <p>
-                      Swap
-                      <!-- <?php echo htmlentities($result->VehiclesOverview);?> -->
-                    </p>
-                  </div>
-                <?php 
-                  }
-                ?>
+              <div role="tabpanel" class="tab-pane" id="swap">                
+                <p>         
+                  <?php
+                  $user_sql = "SELECT id FROM tblusers WHERE EmailId=:email";        
+                  $user_query = $dbh->prepare($user_sql);
+                  $user_query->bindParam(':email', $email, PDO::PARAM_STR);
+                  $user_query->execute();
+                  $user_results = $user_query->fetch();
+
+                  $user_id = $user_results["id"];
+                  $item_status = 1;
+                  
+                  $self_items_sql = "SELECT item.id as itemID, item.user_id, item.swap, item.productName, user.id
+                          FROM tblpostitem as item 
+                          JOIN tblusers as user
+                          ON item.user_id = user.id
+                          WHERE user_id = :user_id AND item.delmode = :item_status";
+                    
+                    $query = $dbh->prepare($self_items_sql);
+                    $query->bindParam(':user_id', $user_id, PDO::PARAM_STR);
+                    $query->bindParam(':item_status', $item_status);
+                    $query->execute();
+                    $results = $query->fetchAll(PDO::FETCH_OBJ);                      
+                    ?>
+                    <h2>Item you have:</h2>
+                    <input type="hidden" name="item_id" id="item_id" value="<?php echo $_GET['vhid'] ?>">
+                    <input type="hidden" name="receiver_id" id="receiver_id" value="<?php echo htmlentities($user_id);?>">
+                    <input type="hidden" name="provider_id" id="provider_id" value="<?php echo htmlentities($providerID);?>">
+                    <select name="receiver_item_id" id="receiver_item_id">
+                      <?php
+                      foreach ($results as $result) {                      
+                      ?>
+                        <option style="background: #346BE0; color: white; padding: 2em; border-radius: 15px;" value="<?php echo htmlentities($result->itemID) ?>"><?php echo htmlentities($result->productName) ?></option>
+                      <?php
+                      }
+                      ?>
+                    </select>                    
+                  <button id="swap-with-owner-btn">Swap with owner</button>
+                </p>
+              </div>
+
+              <!--  -->
 
               <!-- RENT -->
               <?php 
